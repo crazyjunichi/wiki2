80e73457448694a4d49bd0b25a25f0b94339ce57
CrazyGod <crazyjunichi@hotmail.com>
2025-03-14 02:25:25 +0800

    EA0.17

--- docs/_nuxt/qKlROYkM.js
+++
@@ -1,1 +0,0 @@
-const e="EnvDamage",t=[{key:"Dmg_RoofCollapsed",text:"坍塌的屋顶",icon:""},{key:"Dmg_RoofDamage",text:"屋顶损坏",icon:"RoofDamage@288"},{key:"TestDamage1",text:"Terrible damage!",icon:"characters@230"}],a=!0,o={title:e,list:t,useImage:a};export{o as default,t as list,e as title,a as useImage};
