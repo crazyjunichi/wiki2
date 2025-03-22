549bdc9957d1ebc17c0d8b483ad469d7f2f9f920
CrazyGod <crazyjunichi@hotmail.com>
2025-03-23 03:08:34 +0800

    EA0.23; add magic system

---
+++ docs/_nuxt/C2tsXn0o.js
@@ -0,0 +1,1 @@
+import{d as n,i as o,_ as r}from"./3cR8CC86.js";const l=n({__name:"test",async setup(a){let e,t;const _=([e,t]=o(()=>r(()=>import("./DQvYv-o5.js"),[],import.meta.url)),e=await e,t(),e).default;return console.log(_),(s,i)=>null}});export{l as default};
---
+++ docs/_nuxt/DhxyQBYz.js
@@ -0,0 +1,1 @@
+import{d as e,c as n,o,q as s}from"./3cR8CC86.js";const _={class:"text-hint"},a=e({__name:"hint",props:{highlight:{type:Boolean}},setup(c){return(t,p)=>(o(),n("span",_,[s(t.$slots,"default")]))}}),i=Object.assign(a,{__name:"Hint"});export{i as _};
