aba1f787e941caafa335092e375fc1a276aa6550
CrazyGod <crazyjunichi@hotmail.com>
2025-03-28 01:23:18 +0800

    fix

--- docs/_nuxt/D4kRWCiS.js
+++
@@ -1,1 +0,0 @@
-import{d as n,i as o,_ as r}from"./BxI2mxei.js";const l=n({__name:"test",async setup(a){let e,t;const _=([e,t]=o(()=>r(()=>import("./DQQpmBgl.js"),[],import.meta.url)),e=await e,t(),e).default;return console.log(_),(s,i)=>null}});export{l as default};
