b4c0f0a6abdd696007df1434958c24f56b036da6
CrazyGod <crazyjunichi@hotmail.com>
2025-04-29 02:22:16 +0800

    update

--- docs/_nuxt/C5SNP4vQ.js
+++
@@ -1,1 +0,0 @@
-import{d as n,n as o,_ as r}from"./D82hzBR5.js";const l=n({__name:"test",async setup(a){let e,t;const _=([e,t]=o(()=>r(()=>import("./BomK8vWt.js"),[],import.meta.url)),e=await e,t(),e).default;return console.log(_),(s,i)=>null}});export{l as default};
