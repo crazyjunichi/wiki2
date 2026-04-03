51ea3679fa86911255114aec6970c5632d66db45
CrazyGod <crazyjunichi@hotmail.com>
2026-04-04 01:24:01 +0800

    Update

--- docs/_nuxt/DCwkNvqS.js
+++
@@ -1,1 +0,0 @@
-import{d as n,w as o,e as r}from"./BrOzm4h1.js";const l=n({__name:"test",async setup(a){let e,t;const _=([e,t]=o(()=>r(()=>import("./IJFq9wwH.js"),[],import.meta.url)),e=await e,t(),e).default;return console.log(_),(s,i)=>null}});export{l as default};
--- docs/_nuxt/Dp5rcTm0.js
+++
@@ -1,1 +0,0 @@
-import{E as r}from"./BrOzm4h1.js";var e=r();export{e as O};
